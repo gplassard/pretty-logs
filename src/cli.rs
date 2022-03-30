d3089a709869abcfff9352a59761bcc8b1ea2135
Gabriel Plassard <gabriel.plassard@gmail.com>
2022-03-30 21:57:43 +0200

    Reformatting

--- src/cli.rs
+++ src/cli.rs
@@ -1,15 +1,14 @@
 use structopt::{clap, StructOpt};
 
 #[derive(Debug, StructOpt)]
-/// Ignore characters from input in order to transform each line into (hopefully) valid
-/// json Designed to be used in conjunction with https://github.com/TylerBrock/saw and
-/// https://github.com/stedolan/jq) for log produced by an AWS Lambda with NodeJS
-/// runtime.
+/// Ignore characters from input in order to transform each line into (hopefully) valid json.
+/// Designed to be used in conjunction with https://github.com/TylerBrock/saw and
+/// https://github.com/stedolan/jq) for log produced by an AWS Lambda with NodeJS runtime.
 ///
 /// Example usage :
 /// saw watch /aws/lambda/loggroup | pretty-logs | jq
 #[structopt(name = "pretty-logs", setting = clap::AppSettings::DeriveDisplayOrder)]
-#[structopt(set_term_width = 85)]
+#[structopt(set_term_width = 90)]
 pub struct Cli {
     #[structopt(long)]
     /// don't remove characters at the start of the line
