fcb38541840b468c6ca4fd7685a36b5530ebfd6f
Gabriel Plassard <gabriel.plassard@gmail.com>
2022-03-30 21:51:31 +0200

    Support preserve-start & preserve-end

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,21 @@
-use structopt::StructOpt;
+use structopt::{clap, StructOpt};
 
-#[derive(StructOpt)]
-/// Drop input until a '{' character is encountered.
-/// Designed to be used in conjunction with https://github.com/TylerBrock/saw and https://github.com/stedolan/jq) for log produced by an AWS Lambda with NodeJS runtime.
+#[derive(Debug, StructOpt)]
+/// Ignore characters from input in order to transform each line into (hopefully) valid
+/// json Designed to be used in conjunction with https://github.com/TylerBrock/saw and
+/// https://github.com/stedolan/jq) for log produced by an AWS Lambda with NodeJS
+/// runtime.
 ///
 /// Example usage :
 /// saw watch /aws/lambda/loggroup | pretty-logs | jq
-#[structopt(name = "pretty-logs")]
-pub struct Cli {}
+#[structopt(name = "pretty-logs", setting = clap::AppSettings::DeriveDisplayOrder)]
+#[structopt(set_term_width = 85)]
+pub struct Cli {
+    #[structopt(long)]
+    /// don't remove characters at the start of the line
+    pub preserve_start: bool,
+
+    #[structopt(long)]
+    /// don't remove characters at the end of the line
+    pub preserve_end: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use structopt::StructOpt;
 use cli::Cli;
 
 fn main() {
-    let _cli = Cli::from_args();
+    let cli = Cli::from_args();
     if atty::is(atty::Stream::Stdin) {
         exit(0);
     }
@@ -17,7 +17,7 @@ fn main() {
                 if n == 0 {
                     exit(0);
                 }
-                match clean_line(&input) {
+                match clean_line(&input, &cli) {
                     Some(x) => println!("{}", x),
                     None => {}
                 };
@@ -30,25 +30,101 @@ fn main() {
     }
 }
 
-fn clean_line(line: &String) -> Option<&str> {
-    if line.contains('{') {
-        Some(line.trim_start_matches(|c| c != '{'))
-    } else {
-        None
+fn clean_line<'a>(line: &'a str, cli: &Cli) -> Option<&'a str> {
+    let mut index_of_first_bracket = None;
+    let mut index_of_last_bracket = None;
+
+    for (i, c) in line.chars().enumerate() {
+        if c == '{' && index_of_first_bracket.is_none() {
+            index_of_first_bracket = Some(i);
+        } else if c == '}' {
+            index_of_last_bracket = Some(i);
+        }
+    }
+
+    match (cli, index_of_first_bracket, index_of_last_bracket) {
+        (Cli { preserve_start: false, preserve_end: false }, Some(first), Some(last)) => Some(&line[first..(last + 1)]),
+        (Cli { preserve_start: false, preserve_end: true }, Some(first), _) => Some(&line[first..]),
+        (Cli { preserve_start: false, .. }, Some(first), None) => Some(&line[first..]),
+        (Cli { preserve_start: true, .. }, _, Some(last)) => Some(&line[..(last + 1)]),
+        (Cli { preserve_start: true, .. }, _, _) => Some(&line),
+        (_, _, _) => None
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::clean_line;
+    use crate::{clean_line, Cli};
 
     #[test]
-    fn non_matching_string() {
-        assert_eq!(clean_line(&"aaaaa".to_string()), None)
+    fn no_json_no_preserve() {
+        let cli = Cli {
+            preserve_start: false,
+            preserve_end: false,
+        };
+        assert_eq!(clean_line(&"aaaaa", &cli), None)
     }
 
     #[test]
-    fn matching_string() {
-        assert_eq!(clean_line(&"aaaaa {\"count\": 0}".to_string()), Some("{\"count\": 0}"))
+    fn no_json_preserve_start() {
+        let cli = Cli {
+            preserve_start: true,
+            preserve_end: false,
+        };
+        assert_eq!(clean_line(&"aaaaa", &cli), Some("aaaaa"))
+    }
+
+    #[test]
+    fn no_json_preserve_end() {
+        let cli = Cli {
+            preserve_start: false,
+            preserve_end: true,
+        };
+        assert_eq!(clean_line(&"aaaaa", &cli), None)
+    }
+
+    #[test]
+    fn clean_start() {
+        let cli = Cli {
+            preserve_start: false,
+            preserve_end: false,
+        };
+        assert_eq!(clean_line(&"aaaaa {\"count\": 0}", &cli), Some("{\"count\": 0}"))
+    }
+
+    #[test]
+    fn clean_end() {
+        let cli = Cli {
+            preserve_start: false,
+            preserve_end: false,
+        };
+        assert_eq!(clean_line(&"{\"count\": 0} aaaaaaaa", &cli), Some("{\"count\": 0}"))
+    }
+
+    #[test]
+    fn clean_both() {
+        let cli = Cli {
+            preserve_start: false,
+            preserve_end: false,
+        };
+        assert_eq!(clean_line(&"aaaaaaa {\"count\": 0} aaaaaaaa", &cli), Some("{\"count\": 0}"))
+    }
+
+    #[test]
+    fn preserve_start() {
+        let cli = Cli {
+            preserve_start: true,
+            preserve_end: false,
+        };
+        assert_eq!(clean_line(&"aaaaaaa {\"count\": 0} aaaaaaaa", &cli), Some("aaaaaaa {\"count\": 0}"))
+    }
+
+    #[test]
+    fn preserve_end() {
+        let cli = Cli {
+            preserve_start: false,
+            preserve_end: true,
+        };
+        assert_eq!(clean_line(&"aaaaaaaaaaa {\"count\": 0} aaaaaaaa", &cli), Some("{\"count\": 0} aaaaaaaa"))
     }
 }
